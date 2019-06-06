ba14ae37b0ae1d875bc725528e399e24a35fcb2a
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-06-06 17:08:23 +0200

    removed cookie information as this is not required, fixes #4

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,19 +1,8 @@
 $(document).ready(function () {
-	$('.tap-target').tapTarget();
-	$('.tap-target').tapTarget('open');
-	if (localStorage.getItem('cookieconsent') === 'true') {
-		$('#cookies').hide();
-	}
-	$('#cookies').on('click', function(event) {
-		localStorage.setItem('cookieconsent', 'true');
-		$('#cookies a').addClass("scale-out");
-	});
-	
 	$('.fixed-action-btn').floatingActionButton({
 		direction: 'left', 
 		hoverEnabled: false
 	});
-
 
 	$('.sidenav').sidenav();
     $('.parallax').parallax();
